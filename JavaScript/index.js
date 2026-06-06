ecef1eb8c6710bfdba793a6af11eaeb70dfb850c
KARETI PRASANTH KUMAR <prasanthkareti72@gmail.com>
2026-06-06 22:17:01 +0530

    Add JavaScript examples for variables and functions

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,6 +1,41 @@
+day-1
 let name = "Prasanth";
 console.log(name);
 function greet() {
     console.log("Hello JavaScript");
 }
 greet();
+
+day-2
+
+let name = "Prashanth";
+let age = 21;
+
+console.log("Name:", name);
+console.log("Age:", age);
+
+let a = 10;
+let b = 5;
+
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+
+let num = 8;
+
+if (num % 2 === 0) {
+    console.log(num + " is Even");
+} else {
+    console.log(num + " is Odd");
+}
+
+for (let i = 1; i <= 5; i++) {
+    console.log("Count:", i);
+}
+
+function greet(userName) {
+    return "Hello, " + userName;
+}
+
+console.log(greet("Prashanth"));
