e68a1f57d6df05dbcbf5e37f50cd43f67fac4341
Shafi Nihal Shaik <nihalshafi2005@gmail.com>
2026-06-08 15:42:50 +0530

    Update Day 2.js

--- JavaScript/Day 2.js
+++ JavaScript/Day 2.js
@@ -1,1 +1,21 @@
+// 1. Basic Function Declaration
+function addNumbers(a, b) {
+    return a + b;
+}
 
+
+const multiplyNumbers = (a, b) => a * b;
+
+//  Function with a Callback
+function processInput(callback) {
+    const input = "Data received";
+    callback(input);
+}
+
+
+console.log(addNumbers(5, 10));          // Output: 15
+console.log(multiplyNumbers(4, 5));     // Output: 20
+
+processInput((data) => {
+    console.log(`Callback output: ${data}`); 
+});
