9f86b41447a3651ba8aedd29fc59e8750373ad3c
vaishnavi-799 <vaishnavigurramkonda04@gmail.com>
2026-06-10 23:29:28 +0530

    today program

---
+++ JavaScript/Day-4/TypeConv.js.js
@@ -0,0 +1,40 @@
+let n=20;
+let name="Anil";
+let BooleanVal=true;
+let nullVal=null;
+let BigintVal=100n;
+let emptyStr=" ";
+
+console.log(`number: ${n}`);
+console.log(`name: ${name}`);
+console.log(`BooleanVal: ${BooleanVal}`);
+console.log(`nullVal: ${nullVal}`);
+console.log(`BigintVal: ${BigintVal}`);
+console.log(`emptyStr: ${emptyStr}`);
+
+//conver to string
+console.log("=======Coverted to string=======");
+console.log(`number: ${String(n)}`);
+console.log(`name: ${String(name)}`);
+console.log(`BooleanVal: ${String(BooleanVal)}`);
+console.log(`nullVal: ${String(nullVal)}`);
+console.log(`BigintVal: ${String(BigintVal)}`);
+console.log(`emptyStr: ${String(emptyStr)}`);
+
+//conver to Number
+console.log("=======Coverted to Number======");
+console.log(`number: ${Number(n)}`);
+console.log(`name: ${Number(name)}`);
+console.log(`BooleanVal: ${Number(BooleanVal)}`);
+console.log(`nullVal: ${Number(nullVal)}`);
+console.log(`BigintVal: ${Number(BigintVal)}`);
+console.log(`emptyStr: ${Number(emptyStr)}`);
+
+//conver to Boolean
+console.log("=======Coverted to Boolean======");
+console.log(`number: ${Boolean(n)}`);
+console.log(`name: ${Boolean(name)}`);
+console.log(`BooleanVal: ${Boolean(BooleanVal)}`);
+console.log(`nullVal: ${Boolean(nullVal)}`);
+console.log(`BigintVal: ${Boolean(BigintVal)}`);
+console.log(`emptyStr: ${Boolean(emptyStr)}`);
---
+++ JavaScript/Day-5/AgeChecker.js
@@ -0,0 +1,22 @@
+function verifyy()
+{
+    const age=document.getElementById("age").value;
+const name=document.getElementById("name").value;
+const result=document.getElementById("result");
+if(age<18)
+{
+ result.innerHTML= `  You are an Child ${name}`;
+
+}
+else if(age>=18 && age<=20)
+{
+    result.innerHTML = `  You are an Teenager ${name}`;
+}
+else if(age>20 && age<=40)
+{
+    result.innerHTML = `  You are im middle age ${name}`;
+}
+else{
+    result.innerHTML = `  You are an Older ${name}`;
+}
+}
