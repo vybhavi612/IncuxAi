e05de0d94cf08a2b2ae47bb3f61009179a516799
vaishnavi-799 <vaishnavigurramkonda04@gmail.com>
2026-06-10 20:24:51 +0530

    today program

--- JavaScript/Day-1/Function.js
+++ JavaScript/Day-1/Function.js
@@ -1,11 +1,29 @@
-let r=2000
-let s=3000
+let r=2000;
+let s=3000;
 
 function add(r,s)
 {
     console.log("Ramu's Amount",r);
     console.log("somu's Amount",s);
     console.log(`Total Amount ${r+s}`);
+    console.log(typeof(r));
+    console.log(typeof(' '));
 }
 add(r,s);
-alert("Welocme ");
+//alert("Welocme ");
+
+
+let a=prompt("Your name please ");
+console.log(`welcome ${a}`);
+
+// Function: Independent block of code.
+// Method: Function that belongs to a class/object.
+
+
+/* git checkout Vaishnavi-Gurramkonda
+ git status
+ git add .
+ git commit -m "Your message"
+ git pull origin Vaishnavi-Gurramkonda --rebase
+ git push origin Vaishnavi-Gurramkonda
+*/
---
+++ JavaScript/Day-2/DataTypes.js
@@ -0,0 +1,30 @@
+console.log(typeof "Hello");
+console.log(typeof 788);
+console.log(typeof NULL);
+console.log(typeof typeof 788);
+console.log(typeof typeof "hello");
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+console.log(1/0);
+console.log("40"/4); // directly type convert
+console.log("hdbs"/4); //Nan
+console.log(typeof NaN);
+//let max 199254740991
+
+//bigint
+let n1=19925474099177n;
+console.log(typeof n1);
+
+//class of number and bigint
+// Number() BigInt()
+
+
+let f=Boolean(true);
+console.log(f);
+
+//js is dynamically typed language
+let n=90;
+n="dfg dfgh";
+console.log(n);
