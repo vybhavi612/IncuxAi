29b08982023c3325f5aaf19cec5f2b436e7ff7c1
Sai Varshini Iguturi <iguturisaivarshini1806@gmail.com>
2026-06-17 20:11:13 +0530

    Day 2 - Variables

---
+++ index.js
@@ -0,0 +1,7 @@
+let fullName = "Varshini";
+let age = 19;
+let isStudent = true;
+
+document.getElementById("p1").textContent = fullName;
+document.getElementById("p2").textContent = age;
+document.getElementById("p3").textContent = isStudent;
