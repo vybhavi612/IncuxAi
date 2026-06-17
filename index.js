2be243db37cc6ebd384dd3f08d1c7bfc29ebe465
Sai Varshini Iguturi <iguturisaivarshini1806@gmail.com>
2026-06-17 20:12:48 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-let fullName = "Varshini";
-let age = 19;
-let isStudent = true;
-
-document.getElementById("p1").textContent = fullName;
-document.getElementById("p2").textContent = age;
-document.getElementById("p3").textContent = isStudent;
